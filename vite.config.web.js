0d9ddabaaf1c8cf16561799a5acb0ca454bcf2c4
Loïc Combis <combis.loic@gmail.com>
2025-10-15 10:14:51 +0200

    Remove depency to preact

--- vite.config.web.js
+++ vite.config.web.js
@@ -12,14 +12,6 @@ export default defineConfig({
       include: ['src'],
     }),
   ],
-  resolve: {
-    alias: {
-      react: 'preact/compat',
-      'react-dom/test-utils': 'preact/test-utils',
-      'react-dom': 'preact/compat',
-      'react/jsx-runtime': 'preact/jsx-runtime',
-    },
-  },
   build: {
     lib: {
       entry: {
